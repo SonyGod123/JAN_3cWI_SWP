bad85bd6ad3a3a1753f04942feb0c31e2727e0aa
SonyGod123 <jan.tschabrun@hotmail.com>
2021-12-01 09:23:28 +0100

    TicTacToe Anfang while Loop

--- src/at/tsj/projects/TicTacToe/TicTacToe.java
+++ src/at/tsj/projects/TicTacToe/TicTacToe.java
@@ -1,8 +1,12 @@
 package at.tsj.projects.TicTacToe;
 
+import java.util.Scanner;
+
 public class TicTacToe {
     public static void main(String[] args) {
         int[][] Playground = new int [3][3];
+
+        Scanner scanner = new Scanner(System.in);
 
         for (int row = 0; row < Playground.length ; row++) {
             for (int col = 0; col < Playground.length; col++) {
@@ -10,8 +14,19 @@ public class TicTacToe {
             }
             System.out.println();
         }
-        
-        
+
+
+       while (true) {
+           boolean Valid = true;
+
+           while (Valid) {
+               System.out.println("Enter your choice");
+               String input = scanner.next();
+               String[] values = input.split(",");
+           }
+
+
+       }
         
     }
 
