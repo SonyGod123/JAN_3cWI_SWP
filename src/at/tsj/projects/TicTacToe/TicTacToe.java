ce88cc9892b828e2dd1f7da5e90ef8f55b78a93d
SonyGod123 <jan.tschabrun@hotmail.com>
2021-12-16 22:08:08 +0100

    TicTacToe weitergearbeitet

--- src/at/tsj/projects/TicTacToe/TicTacToe.java
+++ src/at/tsj/projects/TicTacToe/TicTacToe.java
@@ -5,24 +5,43 @@ import java.util.Scanner;
 public class TicTacToe {
     public static void main(String[] args) {
         int[][] Playground = new int [3][3];
+        int Player = 0;
 
         Scanner scanner = new Scanner(System.in);
 
-        for (int row = 0; row < Playground.length ; row++) {
-            for (int col = 0; col < Playground.length; col++) {
-                System.out.print(Playground[row][col] + " ");
-            }
-            System.out.println();
-        }
+
 
 
        while (true) {
-           boolean Valid = true;
 
-           while (Valid) {
-               System.out.println("Enter your choice");
-               String input = scanner.next();
-               String[] values = input.split(",");
+           for (int row = 0; row < Playground.length ; row++) {
+               for (int col = 0; col < Playground.length; col++) {
+                   if (Playground[row][col] == 0){
+                       System.out.print("| | ");
+                   }
+                   if (Playground[row][col] == 1){
+                       System.out.print("|x| ");
+                   }
+                   if (Playground[row][col] == 2){
+                       System.out.print("|o| ");
+                   }
+
+               }
+               System.out.println();
+           }
+           System.out.println("Es ist gerade " + (Player%2+1) + " an der Reihe");
+           System.out.println("Geben sie die Spalte an");
+          int Input1 = scanner.nextInt();
+
+           System.out.println("Geben sie die Zeile an");
+           int Input2 = scanner.nextInt();
+
+            if (Playground[Input1][Input2] == 0) {
+                Playground[Input1][Input2] = Player%2+1;
+                Player++;
+            }   else
+                System.out.println("Feld schon belegt!");
+
            }
 
 
@@ -30,4 +49,4 @@ public class TicTacToe {
         
     }
 
-}
+
