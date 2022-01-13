0ce7edd0f92ea21aeea3b12e47e2ecb9b8a20fae
SonyGod123 <jan.tschabrun@hotmail.com>
2022-01-13 19:41:03 +0100

    Objekt Orientierung Anfang

---
+++ src/at/tsj/projects/OO/Auto/Main.java
@@ -0,0 +1,24 @@
+package at.tsj.projects.OO.Auto;
+
+import at.tsj.projects.OO.Auto.objects.Car;
+
+public class Main {
+    public static void main(String[] args) {
+        int a = 7;
+
+        Car c1 = new Car();
+        c1.brand = "Audi";
+        c1.fuelConsumption = 7;
+        c1.serialNumber = "A1234";
+
+
+        Car c2 = new Car();
+        c2.brand = "Mercedes";
+        c2.fuelConsumption = 6;
+        c2.serialNumber = "M1234";
+
+        System.out.println(c1.serialNumber);
+        System.out.println(c2.serialNumber);
+
+    }
+}
---
+++ src/at/tsj/projects/OO/Auto/objects/Car.java
@@ -0,0 +1,12 @@
+package at.tsj.projects.OO.Auto.objects;
+
+public class Car {
+    //Instanz /Gedächtnisveriabelen
+
+    //don't do that later
+    public int fuelConsumption;
+    public String brand;
+    public String serialNumber;
+    private String color;
+
+}
